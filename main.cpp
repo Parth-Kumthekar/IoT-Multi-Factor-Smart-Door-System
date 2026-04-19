435a146c86e8b1b4190278b28314a62639c78535
Parth-Kumthekar <3183509k.Student@gla.ac.uk>
2026-04-19 04:22:05 +0100

    Clean formating for main.cpp

--- main.cpp
+++ main.cpp
@@ -7,26 +7,26 @@
 #include "AccessEvent.h"
 #include "EventBus.h"
 #include "Logger.h"
-#include "DoorController.h"
+#include "DoorLock.h"
 #include "OverrideManager.h"
 #include "CameraThread.h"
 #include "RecognitionThread.h"
 #include "SignalHandler.h"
 #include "GUIServer.h"
 
+
 // Global logger 
 Logger gLogger("database/access_log.csv");
 
 int main() {
+
     installSignalHandlers();
 
-    std::cout << "[Main] FaceID + NFC Door Lock starting\n";
+    std::cout << "FaceID + NFC Door Lock starting\n";
 
     
     DoorController door;       
     GUIServer      gui(8080);   
-
-   
 
     // Camera pipeline
     ThreadSafeQueue<cv::Mat> frameQueue;
@@ -37,7 +37,7 @@ int main() {
     camera.start();
     recognition.start();
 
-    std::cout << "[Main] All subsystems running. "
+    std::cout << "All subsystems running. "
                  "GUI at http://<pi-ip>:8080 | ESC in preview to quit\n";
 
     // Main thread(SIGINT/SIGTERM or ESC)
@@ -46,13 +46,12 @@ int main() {
     }
 
     // Clean shutdown 
-    std::cout << "[Main] Shutting down...\n";
-    recognition.stop();   //
-    camera.stop();        //
-    nfc.stop();           // 
-    gui.stop();           //
+    std::cout << "Shutting down...\n";
+    recognition.stop();   
+    camera.stop();                  
+    gui.stop();           
     
 
-    std::cout << "[Main] Clean exit\n";
+    std::cout << " exit\n";
     return 0;
 }
